ab8162ef8d9d93ddd045e38038aa506d894311d6
JoshnaksPNG <80712017+JoshnaksPNG@users.noreply.github.com>
2022-08-17 00:44:54 -0700

    Arduino Comments and Car Motion

--- arduino/arduino/arduino.ino
+++ arduino/arduino/arduino.ino
@@ -1,36 +1,205 @@
+//Define IO Pins
+#define ENA 6
+#define ENB 5
+#define IN1 7
+#define IN2 8
+#define IN3 9
+#define IN4 11
+
+
+// Setup Function
 void setup() 
 {
-  // put your setup code here, to run once:
+  // Serial Setup
   Serial.begin(9600);
 
+  // Wait For Serial Connection
   while(!Serial)
   {
     ;
   }
 
   Serial.println("Serial Connection Ready");
+
+  //Pin Setups
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
 }
 
+// Main Loop
 void loop() 
 {
-  // put your main code here, to run repeatedly:
+  // Serial Responses
   if (Serial.available() > 0)
   {
     char incoming = Serial.read();
 
     if(incoming == '~')
     {
+      // Node Program Turn On
       Serial.println("Serial Connection Ready");
     } else
     {
+      // Recieving Serial Data
       Serial.print("Recieved: ");
       Serial.println(incoming);
+
+      // Set Drive Variables
+      int drive_code = incoming - '0';
+      int drive_length;
+
+      // Check Drive Codes
+      if(drive_code >= 0 && drive_code <= 7)
+      {
+        // Distinguish Short and Long Drive
+        if(drive_code >=4)
+        {
+          drive_length = 1;
+        } else
+        {
+          drive_length = 2;
+        }
+
+        // Drive
+        switch (drive_code)
+        {
+          case 0:
+            drive_fore(drive_length);
+            break;
+            
+          case 1:
+            drive_back(drive_length);
+            break;
+            
+          case 2:
+            drive_left(drive_length);
+            break;
+            
+          case 3:
+            drive_right(drive_length);
+            break;
+            
+          case 4:
+            drive_fore(drive_length);
+            break;
+            
+          case 5:
+            drive_back(drive_length);
+            break;
+            
+          case 6:
+            drive_left(drive_length);
+            break;
+            
+          case 7:
+            drive_right(drive_length);
+            break;
+        }
+      } else
+      {
+        Serial.println("Unuseable Drive Code");
+      }
     }
-    
   }
 }
 
-void drive_left()
+//Drive Functions
+void drive_left(int drive_length)
 {
+  // Turn On Wheels
+  digitalWrite(ENA, HIGH);
+  digitalWrite(ENB, HIGH);
   
+  // Left Wheels Backward
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+
+  // Right Wheels Foreward
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+
+  // Continue for 500ms * Drive Length ( 1 or 2 )
+  delay(500 * drive_length);
+
+  // Shut Off Wheels
+  digitalWrite(ENA, LOW);
+  digitalWrite(ENB, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN4, LOW);
+}
+
+void drive_right(int drive_length)
+{
+  // Turn On Wheels
+  digitalWrite(ENA, HIGH);
+  digitalWrite(ENB, HIGH);
+  
+  // Left Wheel Foreward
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+
+  // Right Wheel Backward
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+
+  // Continue for 500ms * Drive Length ( 1 or 2 )
+  delay(500 * drive_length);
+
+  // Shut Off Wheels
+  digitalWrite(ENA, LOW);
+  digitalWrite(ENB, LOW);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN3, LOW);
+}
+
+void drive_fore(int drive_length)
+{
+  // Turn On Wheels
+  digitalWrite(ENA, HIGH);
+  digitalWrite(ENB, HIGH);
+  
+  // Left Wheel Foreward
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+
+  // Right Wheel Foreward
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+
+  // Continue for 500ms * Drive Length ( 1 or 2 )
+  delay(500 * drive_length);
+
+  // Shut Off Wheels
+  digitalWrite(ENA, LOW);
+  digitalWrite(ENB, LOW);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN4, LOW);
+}
+
+void drive_back(int drive_length)
+{
+  //Turn On Wheels
+  digitalWrite(ENA, HIGH);
+  digitalWrite(ENB, HIGH);
+  
+  // Left Wheel Backward
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+
+  // Right Wheel Backward
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+
+  // Continue for 500ms * Drive Length ( 1 or 2 )
+  delay(500 * drive_length);
+
+  // Shut Off Wheels
+  digitalWrite(ENA, LOW);
+  digitalWrite(ENB, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
 }
